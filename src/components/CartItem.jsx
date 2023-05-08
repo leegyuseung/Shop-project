cda6a960d8ddb9b2ddc432fcd1aa46985d86c2ca
gyuseung <lgs4002@naver.com>
2023-05-08 22:50:18 +0900

    [feat] 쇼핑카트 CSS

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { addOrUpdateToCart, removeFromCart } from "../api/firebase";
+
+const ICON_CLASS =
+  "transition-all cursor-pointer hover:text-brand hover:scale-105 mx-1";
 export default function CartItem({
   product,
-  product: { id, image, title, option, quantity },
+  product: { id, image, title, option, quantity, price },
   uid,
 }) {
   const handleMinus = () => {
@@ -18,16 +21,19 @@ export default function CartItem({
   const handleDelete = () => removeFromCart(uid, id);
 
   return (
-    <li>
-      <img src={image} alt={title} />
-      <div>
-        <p>{title}</p>
-        <p>{option}</p>
-        <div>
-          <AiOutlineMinusSquare onClick={handleMinus} />
+    <li className="flex justify-between my-2 items-center">
+      <img className="w-24 md:w-48 rounded-lg" src={image} alt={title} />
+      <div className="flex-1 flex justify-between ml-4">
+        <div className="basis-3/5">
+          <p className="text-lg">{title}</p>
+          <p className="text-xl font-bold text-brand">{option}</p>
+          <p>₩{price}</p>
+        </div>
+        <div className="text-2xl flex items-center">
+          <AiOutlineMinusSquare className={ICON_CLASS} onClick={handleMinus} />
           <span>{quantity}</span>
-          <AiOutlinePlusSquare onClick={handlePlus} />
-          <RiDeleteBin5Fill onClick={handleDelete} />
+          <AiOutlinePlusSquare className={ICON_CLASS} onClick={handlePlus} />
+          <RiDeleteBin5Fill onClick={handleDelete} className={ICON_CLASS} />
         </div>
       </div>
     </li>
